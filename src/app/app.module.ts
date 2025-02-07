9f8f73e60179aed6633377b56bb17ff9d4d76609
Ecilapin <alice.litou@gmail.com>
2025-02-07 08:57:31 +0100

    Branche Alice Authentification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,8 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
     HttpClientModule,
     provideFirebaseApp(() => initializeApp({"projectId":"greenhouses-22f8b","appId":"1:208068475780:web:4a07690eb13b678788fbd1","storageBucket":"greenhouses-22f8b.firebasestorage.app","apiKey":"AIzaSyC-Df9l8wPkAuXnu7uJ5mQrCZ186OU64Mg","authDomain":"greenhouses-22f8b.firebaseapp.com","messagingSenderId":"208068475780"})),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()),
+    provideFirebaseApp(() => initializeApp({"projectId":"greenhouses-efa9b","appId":"1:228364984373:web:186b4265c8814a82c4517c","storageBucket":"greenhouses-efa9b.firebasestorage.app","apiKey":"AIzaSyDFQiwuILS9BpR4gR3b8K7Rlb0aMyNNYWI","authDomain":"greenhouses-efa9b.firebaseapp.com","messagingSenderId":"228364984373"}))
   ],
   providers: [],
   bootstrap: [AppComponent]
