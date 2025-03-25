ced9ebe83f61e9a507f5a6042deb674e1b0ac05d
Nathan <nathan.barthelemy2204@gmail.com>
2025-03-25 23:31:48 +0100

    ajout users

--- src/app/greenhouses/greenhouses.component.ts
+++ src/app/greenhouses/greenhouses.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import {  addDoc, collection, doc, Firestore, setDoc } from '@angular/fire/firestore';
+import { Greenhouse } from '../models';
 
 @Component({
   selector: 'app-greenhouses',
   templateUrl: './greenhouses.component.html',
   styleUrl: './greenhouses.component.css'
 })
-export class GreenhousesComponent {
+export class GreenhousesComponent implements OnInit {
+
+  private firestore = inject(Firestore);
+
+  constructor(private auth:Auth) { }
+
+  ngOnInit(): void {
+    this.addUserGreenhouse();
+  }
+
+  public async addUserGreenhouse(){
+    const authUser = this.auth.currentUser;
+    console.log('auth user', authUser?.uid);
+    if (!authUser) {
+      console.error('User not authenticated');
+      throw new Error("User not authenticated");
+    }
+    const userCollectionRef = collection(this.firestore, `Users/${authUser.uid}/Greenhouse`);
+    
+    await addDoc(userCollectionRef,{
+      authUser: authUser.uid,
+    });
+  
+  }
 
 }
