24e51e71ee0990e289676c5504412ed42b3e9ac6
Ecilapin <alice.litou@gmail.com>
2025-03-27 10:45:55 +0100

    GetId

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,12 @@ import { InscriptionComponent } from './inscription/inscription.component';
 import { ConnectionComponent } from './connection/connection.component';
 import { canActivate, redirectLoggedInTo, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 import { MainPageComponent } from './main-page/main-page.component';
-import { TopicComponent } from './topic/topic.component';
+import { TopicsComponent } from './topics/topics.component';
 import { FavoriteComponent } from './favorite/favorite.component';
 import { PersonalGreenhousesComponent } from './personal-greenhouses/personal-greenhouses.component';
 import { PlantsComponent } from './plants/plants.component';
-import { CommentaireComponent } from './commentaire/commentaire.component';
 import { InfosPlantesComponent } from './infos-plantes/infos-plantes.component';
+import { TopicComponent } from './topic/topic.component';
 
 const redirectLoggedInToItems = () => redirectLoggedInTo(['main-page']);
 const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['inscription']);
@@ -20,13 +20,12 @@ const routes: Routes = [
   { path: 'inscription' , component: InscriptionComponent, ...canActivate(redirectLoggedInToItems)},
   { path: '', component: ConnectionComponent, ...canActivate(redirectLoggedInToItems)},
   { path: 'main-page', component: MainPageComponent, ...canActivate(redirectUnauthorizedToLogin)},
-  { path: 'landingpages', component: LandingpagesComponent, ...canActivate(redirectLoggedInToItems)},
-  { path: 'topics', component: TopicComponent, ...canActivate(redirectUnauthorizedToLogin)},
+  { path: 'topics', component: TopicsComponent, ...canActivate(redirectUnauthorizedToLogin)},
   { path: 'favorite', component: FavoriteComponent, ...canActivate(redirectUnauthorizedToLogin)},
   { path: 'greenhouses', component: PersonalGreenhousesComponent, ...canActivate(redirectUnauthorizedToLogin)},
   { path: 'plants', component: PlantsComponent, ...canActivate(redirectUnauthorizedToLogin)},
-  { path: 'comments', component: CommentaireComponent, ...canActivate(redirectUnauthorizedToLogin)},
-    { path: 'infosPlantes', component: InfosPlantesComponent, ...canActivate(redirectUnauthorizedToLogin)}
+  { path: 'infosPlantes/:id', component: InfosPlantesComponent, ...canActivate(redirectUnauthorizedToLogin)},
+  { path: 'topic/:id', component: TopicComponent, ...canActivate(redirectUnauthorizedToLogin) }
   ];
 
 
--- src/app/infos-plantes/infos-plantes.component.ts
+++ src/app/infos-plantes/infos-plantes.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../services/api.service';
+import { Plant } from '../models';
 
 @Component({
   selector: 'app-infos-plantes',
   templateUrl: './infos-plantes.component.html',
   styleUrl: './infos-plantes.component.css'
 })
-export class InfosPlantesComponent {
-
+export class InfosPlantesComponent implements OnInit{
+  constructor(private route : ActivatedRoute, private ApiService : ApiService){}
+  public currentPlant : Plant | undefined;
+  public id: number = 0;
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.id = +params['id'];
+      console.log(this.id);
+      this.ApiService.getPlantByID(this.id).subscribe(plant=>this.currentPlant=plant);
+      console.log(this.currentPlant);
+   });
+  }
 }
